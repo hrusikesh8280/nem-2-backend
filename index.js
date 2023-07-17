14a9b5c29d8ce6ee7f8ac25c1333c848b0075a51
hrusikesh8280 <hrusikeshviroot@gmail.com>
2023-07-17 15:42:48 +0530

    assessment-5 backend

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.get("/",(req,res)=>{
+    res.send(`<p>Hello</p>`)
+})
+
 
 app.use("/api/boards", boardRoutes);
 app.use("/api/tasks", taskRoutes);
